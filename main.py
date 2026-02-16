5f871181a9844f7e852b47b169a906ab186b8185
graive999 <tdh679@gmail.com>
2026-02-16 16:47:43 -0500

    Token metadata

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 
-# 8. Use the generate_content method with the required named parameters
+# Use the generate_content method with the required named parameters
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
 
-# 9. Print the .text property of the response object
-print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+# Print the .text property of the response object
+print(f"Response: {response.text}")
 
 def main():
     print("Hello from aiagent!")
