1c9a8aa424c2538785383cf3f7affef9cd483271
graive999 <tdh679@gmail.com>
2026-02-16 17:13:47 -0500

    User input

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-
-# Use the generate_content method with the required named parameters
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-# Print the .text property of the response object
-print(f"Response: {response.text}")
-
 def main():
     print("Hello from aiagent!")
+
+
+
+
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=args.user_prompt
+    )
+    if response.usage_metadata is None:
+        raise RuntimeError("API request failed: No usage metadata returned")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+    print(f"Response: {response.text}")
+
+
 
 
 if __name__ == "__main__":
