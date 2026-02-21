ac70632aa49578c69d05e9f2eef5f123a465aec2
graive999 <tdh679@gmail.com>
2026-02-21 04:59:42 -0500

    Agent loop

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import function_map 
 from call_function import call_function
 from call_function import available_functions
 
@@ -29,42 +28,49 @@ def main():
     client = genai.Client(api_key=api_key)
 
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt)
-    )
-    if response.usage_metadata is None:
-        raise RuntimeError("API request failed: No usage metadata returned")
 
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    for _ in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt
+            )                
+        )
+    
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+    
+    
+        if response.usage_metadata is None:
+            raise RuntimeError("API request failed: No usage metadata returned")
 
 
-    if response.function_calls:
-        function_results = []
-        for fc in response.function_calls:
-            result_content = call_function(fc, verbose=args.verbose)
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+        if response.function_calls:
+            function_results = []
+            for fc in response.function_calls:
+                result_content = call_function(fc, verbose=args.verbose)
         
-            if (
-                not result_content.parts 
-                or not result_content.parts[0].function_response
-                or result_content.parts[0].function_response.response is None
-            ):
-                raise Exception("Invalid function response structure")
-             
-            function_results.append(result_content.parts[0])
-        
-            if args.verbose:
-                print(f"-> {result_content.parts[0].function_response.response}")
+                
+                function_results.append(result_content.parts[0])
+
+            messages.append(types.Content(role="user", parts=function_results))
+            
+        else:
+            print(f"Final response:\n{response.text}")
+            break
 
     else:
-        print(response.text)
+        print("Error: Agent reached maximum iteration limit.")
+        exit(1)
 
 
 if __name__ == "__main__":
